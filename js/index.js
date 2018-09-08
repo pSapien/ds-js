387e013e34cf7034c91bfca3d732378347fd5945
Unsung-Crusader <prateekthapa26@gmail.com>
2018-09-08 21:40:31 +0545

    added tests for stack

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+import test from './test';
 import Stack from './Stack';
+
+const myStack = new Stack();
+
+myStack.push('JS');
+myStack.push('is');
+myStack.push('boring');
+
+test('testing peek function', { expected: myStack.peek(), result: 'boring' });
+test('testing size function', { expected: myStack.size(), result: 3 });
+test('testing pop function', { expected: myStack.pop(), result: 'boring' });
+myStack.push('awesome');
+test('testing push function', { expected: myStack.peek(), result: 'awesome' });
---
+++ js/test.js
@@ -0,0 +1,9 @@
+export default function test(messageParams, values) {
+  const { expected, result } = values;
+
+  if (expected === result) {
+    console.log(`Test passed => ${messageParams}`);
+  } else {
+    console.error(`Test failed => ${messageParams}`);
+  }
+}
